53731da6dfacdcf2958fb06dabf01c225a20fa50
Gulzina <gulzina.kylychbekkyzy.kk@gmail.com>
2022-06-25 04:54:48 +0600

    First commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+alert("Я изучаю JS");
+console.log("Мое первое сообщение в консоли");
+document.write("<h1>Привет!</h1>" + "<p>Это мой первый скрипт</p>");
+
+console.log("Gulzina " + "Kylychbekova");
+console.log(100 + 100);
+document.write("100 + 100 = " + (100 + 100));
